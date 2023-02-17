adbc03c72df6b099df5d899059fd35a72af6db12
David J. Bradshaw <dave@bradshaw.net>
2023-02-17 13:28:39 +0000

    Reconnect on 500, 503 remote errors

--- src/api.ts
+++ src/api.ts
@@ -37,7 +37,7 @@ export const createClient = async ({ username, password }) => {
         return Promise.resolve();
       }),
     {
-      statusCodes: [400, 401],
+      statusCodes: [400, 401, 500, 503],
     },
   );
 
