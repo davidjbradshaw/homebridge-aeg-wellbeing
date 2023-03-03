38785af9c62b7068f7acb1ffea9b845a05af7df6
David J. Bradshaw <dave@bradshaw.net>
2023-03-03 12:38:03 +0000

    Update deps

--- src/api.ts
+++ src/api.ts
@@ -1,53 +1,53 @@
-import axios from 'axios';
-import createAuthRefreshInterceptor from 'axios-auth-refresh';
+import axios from 'axios'
+import createAuthRefreshInterceptor from 'axios-auth-refresh'
 
-const BASE_URL = 'https://api.delta.electrolux.com/api';
+const BASE_URL = 'https://api.delta.electrolux.com/api'
 const CLIENT_URL =
-  'https://electrolux-wellbeing-client.vercel.app/api/mu52m5PR9X';
+  'https://electrolux-wellbeing-client.vercel.app/api/mu52m5PR9X'
 
 const contentType = {
-  'Content-Type': 'application/json',
+  'Content-Type': 'application/json'
 }
 
 const fetchClientToken = async () => {
   const response = await axios.get(CLIENT_URL, {
-    headers: contentType,
-  });
+    headers: contentType
+  })
 
-  return response.data.accessToken;
-};
+  return response.data.accessToken
+}
 
 const doLogin = async ({ username, password, clientToken }) =>
   axios.post(
     `${BASE_URL}/Users/Login`,
     {
       Username: username,
-      password,
+      password
     },
     {
       headers: {
         ...contentType,
-        Authorization: `Bearer ${clientToken}`,
-      },
-    },
-  );
+        Authorization: `Bearer ${clientToken}`
+      }
+    }
+  )
 
-export default async ({ username, password }) => {
-  const clientToken = await fetchClientToken();
+export default async ({ username, password, log }) => {
+  const clientToken = await fetchClientToken()
   const response = await doLogin({
     username,
     password,
-    clientToken,
-  });
-  const { accessToken } = response.data;
+    clientToken
+  })
+  const { accessToken } = response.data
 
   const client = axios.create({
     baseURL: BASE_URL,
     headers: {
       ...contentType,
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
 
   createAuthRefreshInterceptor(
     client,
@@ -55,17 +55,22 @@ export default async ({ username, password }) => {
       doLogin({
         username,
         password,
-        clientToken,
-      }).then((tokenRefreshResponse) => { // eslint-disable-line promise/always-return
-        client.defaults.headers.common.Authorization = `Bearer ${tokenRefreshResponse.data.accessToken}`;
-        // eslint-disable-next-line no-param-reassign
-        failedRequest.response.config.headers.Authorization = `Bearer ${tokenRefreshResponse.data.accessToken}`;
-      }),
+        clientToken
+      })
+        // eslint-disable-next-line promise/always-return
+        .then((tokenRefreshResponse) => {
+          // eslint-disable-line promise/always-return
+          client.defaults.headers.common.Authorization = `Bearer ${tokenRefreshResponse.data.accessToken}`
+          // eslint-disable-next-line no-param-reassign
+          failedRequest.response.config.headers.Authorization = `Bearer ${tokenRefreshResponse.data.accessToken}`
+        })
+        .catch((error) => {
+          log.error('Axios:', error)
+        }),
     {
-      statusCodes: [400, 401, 403, 408, 429, 500, 502, 503, 504],
-    },
-  );
+      statusCodes: [400, 401, 403, 408, 429, 500, 502, 503, 504]
+    }
+  )
 
-  return client;
-};
-
+  return client
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,184 +1,184 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import homebridge from 'homebridge';
+import homebridge from 'homebridge'
 
-declare type Username = string;
-declare type Password = string;
-declare type ClientSecret = string;
+declare type Username = string
+declare type Password = string
+declare type ClientSecret = string
 
 declare module 'homebridge' {
   export interface AccessoryConfig {
-    clientSecret: ClientSecret;
-    username: Username;
-    password: Password;
+    clientSecret: ClientSecret
+    username: Username
+    password: Password
   }
 }
 
 export const enum WorkModes {
   Off = 'PowerOff',
   Manual = 'Manual',
-  Auto = 'Auto',
+  Auto = 'Auto'
 }
 
 export const enum ConnectionState {
   Connected = 'Connected',
-  Disconnected = 'Disconnected',
+  Disconnected = 'Disconnected'
 }
 
 interface Appliance {
-  pncId: string;
-  name: string;
-  modelName: string;
-  firmwareVersion: string;
-  workMode: WorkModes;
-  filterRFID: string;
-  filterLife: number;
-  fanSpeed: number;
-  UILight: boolean;
-  safetyLock: boolean;
-  ionizer: boolean;
-  sleep?: boolean;
-  scheduler?: boolean;
-  filterType: number;
-  version: number;
-  doorOpen: boolean;
-  pm1: number;
-  pm25: number;
-  pm10: number;
-  tvoc: number;
-  co2: number;
-  temp: number;
-  humidity: number;
-  envLightLevel?: number;
-  rssi: number;
+  pncId: string
+  name: string
+  modelName: string
+  firmwareVersion: string
+  workMode: WorkModes
+  filterRFID: string
+  filterLife: number
+  fanSpeed: number
+  UILight: boolean
+  safetyLock: boolean
+  ionizer: boolean
+  sleep?: boolean
+  scheduler?: boolean
+  filterType: number
+  version: number
+  doorOpen: boolean
+  pm1: number
+  pm25: number
+  pm10: number
+  tvoc: number
+  co2: number
+  temp: number
+  humidity: number
+  envLightLevel?: number
+  rssi: number
 }
 
 declare namespace WellbeingApi {
   export interface Appliance {
-    applianceName: string;
-    created: string;
-    id: number;
-    modelName: string;
-    timeZoneStandardName: string;
-    pncId: string;
-    domainId: number;
+    applianceName: string
+    created: string
+    id: number
+    modelName: string
+    timeZoneStandardName: string
+    pncId: string
+    domainId: number
   }
 
   export interface ApplianceData {
-    pncId: string;
-    applianceData: ApplianceDataData;
-    twin: Twin;
-    telemetry: null;
+    pncId: string
+    applianceData: ApplianceDataData
+    twin: Twin
+    telemetry: null
   }
 
   export interface ApplianceDataData {
-    applianceName: string;
-    created: string;
-    modelName: string;
-    pncId: string;
+    applianceName: string
+    created: string
+    modelName: string
+    pncId: string
   }
 
   export interface Twin {
-    deviceId: string;
-    properties: Properties;
-    status: string;
-    connectionState: ConnectionState;
+    deviceId: string
+    properties: Properties
+    status: string
+    connectionState: ConnectionState
   }
 
   export interface Properties {
-    desired: Desired;
-    reported: Reported;
+    desired: Desired
+    reported: Reported
   }
 
   export interface Desired {
-    TimeZoneStandardName: string;
-    LocationReq: boolean;
-    FrmVer_NIU: string;
-    $metadata: DesiredMetadata;
-    $version: number;
+    TimeZoneStandardName: string
+    LocationReq: boolean
+    FrmVer_NIU: string
+    $metadata: DesiredMetadata
+    $version: number
   }
 
   export interface DesiredMetadata {
-    $lastUpdated: string;
-    $lastUpdatedVersion: number;
-    TimeZoneStandardName: LastUpdatedWithVersion;
-    LocationReq: LastUpdatedWithVersion;
-    FrmVer_NIU: LastUpdatedWithVersion;
+    $lastUpdated: string
+    $lastUpdatedVersion: number
+    TimeZoneStandardName: LastUpdatedWithVersion
+    LocationReq: LastUpdatedWithVersion
+    FrmVer_NIU: LastUpdatedWithVersion
   }
 
   export interface LastUpdatedWithVersion {
-    $lastUpdated: string;
-    $lastUpdatedVersion: number;
+    $lastUpdated: string
+    $lastUpdatedVersion: number
   }
 
   export interface Reported {
-    FrmVer_NIU: string;
-    Workmode: WorkModes;
-    FilterRFID: string;
-    FilterLife: number;
-    Fanspeed: number;
-    UILight: boolean;
-    SafetyLock: boolean;
-    Ionizer: boolean;
-    Sleep?: boolean;
-    Scheduler?: boolean;
-    FilterType: number;
-    DspIcoTVOC: boolean;
-    ErrPM2_5: boolean;
-    ErrTVOC: boolean;
-    ErrTempHumidity: boolean;
-    ErrFanMtr: boolean;
-    ErrCommSensorDisplayBrd: boolean;
-    DoorOpen: boolean;
-    ErrRFID: boolean;
-    SignalStrength: string;
-    $metadata: ReportedMetadata;
-    $version: number;
-    deviceId: string;
-    TVOC: number;
-    CO2: number;
-    PM1: number;
-    PM2_5: number;
-    PM10: number;
-    Humidity: number;
-    Temp: number;
-    RSSI: number;
-    EnvLightLvl?: number;
+    FrmVer_NIU: string
+    Workmode: WorkModes
+    FilterRFID: string
+    FilterLife: number
+    Fanspeed: number
+    UILight: boolean
+    SafetyLock: boolean
+    Ionizer: boolean
+    Sleep?: boolean
+    Scheduler?: boolean
+    FilterType: number
+    DspIcoTVOC: boolean
+    ErrPM2_5: boolean
+    ErrTVOC: boolean
+    ErrTempHumidity: boolean
+    ErrFanMtr: boolean
+    ErrCommSensorDisplayBrd: boolean
+    DoorOpen: boolean
+    ErrRFID: boolean
+    SignalStrength: string
+    $metadata: ReportedMetadata
+    $version: number
+    deviceId: string
+    TVOC: number
+    CO2: number
+    PM1: number
+    PM2_5: number
+    PM10: number
+    Humidity: number
+    Temp: number
+    RSSI: number
+    EnvLightLvl?: number
   }
 
   export interface ReportedMetadata {
-    $lastUpdated: string;
-    FrmVer_NIU: LastUpdated;
-    Workmode: LastUpdated;
-    FilterRFID: LastUpdated;
-    FilterLife: LastUpdated;
-    Fanspeed: LastUpdated;
-    UILight: LastUpdated;
-    SafetyLock: LastUpdated;
-    Ionizer: LastUpdated;
-    Sleep?: LastUpdated;
-    Scheduler?: LastUpdated;
-    FilterType: LastUpdated;
-    DspIcoTVOC: LastUpdated;
-    ErrPM2_5: LastUpdated;
-    ErrTVOC: LastUpdated;
-    ErrTempHumidity: LastUpdated;
-    ErrFanMtr: LastUpdated;
-    ErrCommSensorDisplayBrd: LastUpdated;
-    DoorOpen: LastUpdated;
-    ErrRFID: LastUpdated;
-    SignalStrength: LastUpdated;
-    TVOC: LastUpdated;
-    CO2: LastUpdated;
-    PM1: LastUpdated;
-    PM2_5: LastUpdated;
-    PM10: LastUpdated;
-    Humidity: LastUpdated;
-    Temp: LastUpdated;
-    RSSI: LastUpdated;
-    EnvLightLvl?: LastUpdated;
+    $lastUpdated: string
+    FrmVer_NIU: LastUpdated
+    Workmode: LastUpdated
+    FilterRFID: LastUpdated
+    FilterLife: LastUpdated
+    Fanspeed: LastUpdated
+    UILight: LastUpdated
+    SafetyLock: LastUpdated
+    Ionizer: LastUpdated
+    Sleep?: LastUpdated
+    Scheduler?: LastUpdated
+    FilterType: LastUpdated
+    DspIcoTVOC: LastUpdated
+    ErrPM2_5: LastUpdated
+    ErrTVOC: LastUpdated
+    ErrTempHumidity: LastUpdated
+    ErrFanMtr: LastUpdated
+    ErrCommSensorDisplayBrd: LastUpdated
+    DoorOpen: LastUpdated
+    ErrRFID: LastUpdated
+    SignalStrength: LastUpdated
+    TVOC: LastUpdated
+    CO2: LastUpdated
+    PM1: LastUpdated
+    PM2_5: LastUpdated
+    PM10: LastUpdated
+    Humidity: LastUpdated
+    Temp: LastUpdated
+    RSSI: LastUpdated
+    EnvLightLvl?: LastUpdated
   }
 
   export interface LastUpdated {
-    $lastUpdated: string;
+    $lastUpdated: string
   }
 }
